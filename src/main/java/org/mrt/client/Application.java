c27683a64bcbb3c632c95ac4e7b8a2a0c1c285ac
Arthur Kalmenson <arthur_kalmenson@otpp.com>
2010-09-22 13:27:28 -0400

    * started to do some cleanup of the generated code. * made the HTML document an HTML 5 doctype.

--- src/main/java/org/mrt/client/Application.java
+++ src/main/java/org/mrt/client/Application.java
@@ -4,20 +4,16 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
-
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class Application
-    implements EntryPoint
-{
+public class Application implements EntryPoint {
 
-  /**
-   * This is the entry point method.
-   */
-  public void onModuleLoad()
-  {
-     final Label label = new Label ( "gwt-maven-plugin Archetype :: Project org.codehaus.mojo.gwt-maven-plugin" );
-     RootPanel.get().add( label );
-  }
+    /**
+     * This is the entry point method.
+     */
+    public void onModuleLoad() {
+        final Label label = new Label("gwt-maven-plugin Archetype :: Project org.codehaus.mojo.gwt-maven-plugin");
+        RootPanel.get().add(label);
+    }
 }
--- src/test/java/org/mrt/client/GwtTestSample.java
+++ src/test/java/org/mrt/client/GwtTestSample.java
@@ -4,20 +4,16 @@ import junit.framework.Assert;
 
 import com.google.gwt.junit.client.GWTTestCase;
 
-public class GwtTestSample
-    extends GWTTestCase
-{
+public class GwtTestSample extends GWTTestCase {
 
-   public String getModuleName()
-   {
-      return "org.mrt.Application";
-   }
+    public String getModuleName() {
+        return "org.mrt.Application";
+    }
 
-   public void testSomething()
-   {
-      // Not much to actually test in this sample app
-      // Ideally you would test your Controller here (NOT YOUR UI)
-      // (Make calls to RPC services, test client side model objects, test client side logic, etc)
-      Assert.assertTrue( true );
-   }
+    public void testSomething() {
+        // Not much to actually test in this sample app
+        // Ideally you would test your Controller here (NOT YOUR UI)
+        // (Make calls to RPC services, test client side model objects, test client side logic, etc)
+        Assert.assertTrue(true);
+    }
 }
