44977fb72adb1bbedd482a4f637b1a5e0c1896a3
Arthur Kalmenson <arthur.kalm@gmail.com>
2010-09-22 20:08:57 -0400

    * renamed the archetype generated classes to MavenReleaseTool.

--- src/main/java/org/mrt/client/MavenReleaseTool.java
+++ src/main/java/org/mrt/client/MavenReleaseTool.java
@@ -7,13 +7,13 @@ import com.google.gwt.user.client.ui.RootPanel;
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class Application implements EntryPoint {
+public class MavenReleaseTool implements EntryPoint {
 
     /**
      * This is the entry point method.
      */
     public void onModuleLoad() {
-        final Label label = new Label("gwt-maven-plugin Archetype :: Project org.codehaus.mojo.gwt-maven-plugin");
+        final Label label = new Label("Maven Release Tool");
         RootPanel.get().add(label);
     }
 }
