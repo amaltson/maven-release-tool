c47c4db69302ba7281e18dbf3ecf178a5abbf991
Arthur Kalmenson <arthur.kalm@gmail.com>
2010-10-01 23:26:36 -0400

    finished defining the domain layer. Closes #7.
    * added the MavenTree implementation. This will probably change soon.

--- src/main/java/org/mrt/shared/MavenTree.java
+++ src/main/java/org/mrt/shared/MavenTree.java
@@ -1,5 +1,29 @@
 package org.mrt.shared;
 
-public class MavenTree {
+import java.util.Map;
 
+import com.google.gwt.dev.util.collect.HashMap;
+
+/**
+ * A container for a maven dependency tree, contains the root node and a list of
+ * dependencies.
+ * 
+ * @author Arthur Kalmenson
+ */
+public class MavenTree {
+	private MavenModule root;
+	private Map<MavenTarget, MavenModule> dependencies;
+
+	public MavenTree(MavenModule root) {
+		this.root = root;
+		dependencies = new HashMap<MavenTarget, MavenModule>();
+	}
+
+	public MavenModule getRoot() {
+		return root;
+	}
+
+	public MavenModule getDependency(MavenTarget target) {
+		return dependencies.get(target);
+	}
 }
